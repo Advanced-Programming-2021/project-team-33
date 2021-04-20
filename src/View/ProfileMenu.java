73a1031f02b31fb4db23decdec5b3fb4486b6143
Aliiihezaveh <ahezaveh554@gmail.com>
2021-04-20 17:00:47 +0430

    update ProfileMenu.java

--- src/View/ProfileMenu.java
+++ src/View/ProfileMenu.java
@@ -8,13 +8,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ProfileMenu {
+    private static String playerName;
     public static Player player;
 
     ProfileMenu() {
 
     }
 
-    public static void manage() {
+    public static void manage(String currentName) {
+        playerName = currentName;
         ProgramController programController = new ProgramController();
         Scanner scanner = programController.scanner;
         Pattern pattern;
@@ -29,14 +31,16 @@ public class ProfileMenu {
                 }
             }
             if (input.matches("profile change --password --current <[.]*> --new <[.]*>")) {
-                pattern=Pattern.compile("profile change --password --current <([.]*)> --new <([.]*)>");
-                matcher= pattern.matcher(input);
-                if(matcher.find()){
-                    changePassword(matcher.group(1),matcher.group(2));
+                pattern = Pattern.compile("profile change --password --current <([.]*)> --new <([.]*)>");
+                matcher = pattern.matcher(input);
+                if (matcher.find()) {
+                    changePassword(matcher.group(1), matcher.group(2));
                 }
             }
+            if(input.matches("exit menu")){
+                exitMenu();
+            }
         }
-
     }
 
     public static Player getPlayer(String name) {
@@ -53,33 +57,43 @@ public class ProfileMenu {
         if (isNameExist(newName)) {
             System.out.println("user with nickname<" + newName + "> already exists");
         } else {
-            System.out.println("nickname changed successfully");
+            System.out.println("nickname changed successfully!");
             player.setNickname(newName);
+            playerName = newName;
         }
     }
 
-    public static void changePassword(String currentPassword,String newPassword) {
-        if(isPasswordTrue(currentPassword)){
-
-        }
-        else {
-
+    public static void changePassword(String currentPassword, String newPassword) {
+        if (isPasswordTrue(currentPassword)) {
+            if(isPasswordEqual(newPassword)){
+                System.out.println("please enter a new password");
+            }
+            else {
+                System.out.println("password changed successfully!");
+                player = getPlayer(playerName);
+                player.setPassword(newPassword);
+            }
+        } else {
+            System.out.println("current password is invalid");
         }
     }
 
     public static String getUserPassword() {
-        return "A";
+        player = Player.getUserByUsername(playerName);
+        return player.getPassword();
     }
 
     public static boolean isPasswordTrue(String currentPassword) {
-        return true;
+        if (getUserPassword().equals(currentPassword))
+            return true;
+        return false;
     }
 
     public static boolean isPasswordEqual(String newPassword) {
-        return true;
+        if(newPassword.equals(getUserPassword())) return true;
+        return false;
     }
 
     public static void exitMenu() {
-
     }
 }
